2aefdf7a7bb456bf00774979702fee24e9e2a1d3
zun <k5orkrj@gmail.com>
2025-01-11 05:49:44 +0900

    feat: implement AttributeConverter to support VO mapping in ShowtimeEntity

--- domain/src/main/java/hanghae/domain/common/showtime/ScheduleConverter.java
+++ domain/src/main/java/hanghae/domain/common/showtime/ScheduleConverter.java
@@ -1,2 +1,39 @@
-package hanghae.domain.common.showtime;public class ScheduleConverter {
+package hanghae.domain.common.showtime;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter(autoApply = true)
+public class ScheduleConverter implements AttributeConverter<Schedule, String> {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public String convertToDatabaseColumn(Schedule schedule) {
+
+        if (schedule == null) {
+            return null;
+        }
+
+        try {
+            return objectMapper.writeValueAsString(schedule);
+        } catch (JsonProcessingException e) {
+            throw new IllegalArgumentException("failed to convert Schedule to JSON", e);
+        }
+    }
+
+    @Override
+    public Schedule convertToEntityAttribute(String dbData) {
+
+        if (dbData == null || dbData.isEmpty()) {
+            return null;
+        }
+
+        try {
+            return objectMapper.readValue(dbData, Schedule.class);
+        } catch (JsonProcessingException e) {
+            throw new IllegalArgumentException("failed to convert JSON to Schedule", e);
+        }
+    }
 }
