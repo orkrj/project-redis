8d171bad254e5a2cd23426692698bdce5402abeb
zun <k5orkrj@gmail.com>
2025-01-11 06:19:59 +0900

    feat: create draft of TheaterEntity

--- domain/src/main/java/hanghae/domain/entity/TheaterEntity.java
+++ domain/src/main/java/hanghae/domain/entity/TheaterEntity.java
@@ -1,2 +1,20 @@
-package hanghae.domain.entity;public class TheaterEntity {
+package hanghae.domain.entity;
+
+import hanghae.domain.common.theater.ShowtimeCollection;
+import hanghae.domain.common.theater.ShowtimeCollectionConverter;
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+public class TheaterEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long theaterId;
+
+    private String theaterName;
+
+    @Convert(converter = ShowtimeCollectionConverter.class)
+    private ShowtimeCollection showtimeCollection;
 }
