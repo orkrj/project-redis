c067b6e1462fc500431e2c6c72a2e598feb654d6
zun <k5orkrj@gmail.com>
2025-01-11 06:36:16 +0900

    feat: create draft of TheaterShowtimeEntity for many to many relation

--- domain/src/main/java/hanghae/domain/entity/TheaterShowtimeEntity.java
+++ domain/src/main/java/hanghae/domain/entity/TheaterShowtimeEntity.java
@@ -1,2 +1,22 @@
-package hanghae.domain.entity;public class TheaterShowtimeEntity {
+package hanghae.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "theater_showtime")
+@NoArgsConstructor
+public class TheaterShowtimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long theaterShowtimeId;
+
+    @ManyToOne
+    @JoinColumn(name = "theater_id")
+    private TheaterEntity theater;
+
+    @ManyToOne
+    @JoinColumn(name = "showtime_id")
+    private ShowtimeEntity showtime;
 }
