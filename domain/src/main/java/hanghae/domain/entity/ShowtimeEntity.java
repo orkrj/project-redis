add4f37dcc19e3e4b494d122ebb73c71612b7693
zun <k5orkrj@gmail.com>
2025-01-11 06:20:59 +0900

    feat: create draft of ShowtimeEntity

--- domain/src/main/java/hanghae/domain/entity/ShowtimeEntity.java
+++ domain/src/main/java/hanghae/domain/entity/ShowtimeEntity.java
@@ -1,2 +1,24 @@
-package hanghae.domain.entity;public class ShowtimeEntity {
+package hanghae.domain.entity;
+
+import hanghae.domain.common.showtime.Schedule;
+import hanghae.domain.common.showtime.ScheduleConverter;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "showtimes")
+public class ShowtimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long showtimeId;
+
+    @Convert(converter = ScheduleConverter.class)
+    private Schedule schedule;
+
+    @OneToOne
+    @JoinColumn(name = "movie_id")
+    private MovieEntity movie;
+
+    // TODO 다대다 풀기 위해 중간 테이블 필요
+    // private TheaterEntity theater;
 }
