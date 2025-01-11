1dcc6f54b1feb137e6402dd7a8b54c072e8f55be
zun <k5orkrj@gmail.com>
2025-01-11 10:07:39 +0900

    feat: update query to find movies playing now

--- infrastructure/src/main/java/hanghae/infrastructure/repository/JpaMovieRepository.java
+++ infrastructure/src/main/java/hanghae/infrastructure/repository/JpaMovieRepository.java
@@ -2,11 +2,17 @@ package hanghae.infrastructure.repository;
 
 import hanghae.domain.entity.MovieEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 public interface JpaMovieRepository extends JpaRepository<MovieEntity, Long> {
-    Optional<List<MovieEntity>> findMoviesPlayingNow(LocalDate now);
+    @Query("SELECT m FROM MovieEntity m " +
+            "WHERE m.releaseDate <= :now " +
+            "AND (m.deletedAt IS NULL OR m.deletedAt > :now)")
+    // 상영 중인 영화 -> release_date <= now <= deleted_at
+    Optional<List<MovieEntity>> findMoviesPlayingNow(@Param("now") LocalDate now);
 }
