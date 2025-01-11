100f4546a625a27615d18ff82c7989aa2e90f89b
zun <k5orkrj@gmail.com>
2025-01-11 09:06:23 +0900

    feat: (temp) add rest-controller to find movies which is playing now

--- api/src/main/java/hanghae/api/adapter/inbound/MovieController.java
+++ api/src/main/java/hanghae/api/adapter/inbound/MovieController.java
@@ -1,2 +1,24 @@
-package hanghae.api.adapter.inbound;public class MovieController {
+package hanghae.api.adapter.inbound;
+
+import hanghae.application.dto.MovieResponse;
+import hanghae.application.port.inbound.MovieService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/movie")
+public class MovieController {
+
+    private final MovieService movieService;
+
+    @GetMapping("/now")
+    public ResponseEntity<List<MovieResponse>> getMoviesPlayingNow() {
+        return ResponseEntity.ok(movieService.findMoviesPlayingNow());
+    }
 }
