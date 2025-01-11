bc5d4b5591c65bf562327b05592564196a986140
zun <k5orkrj@gmail.com>
2025-01-11 09:08:13 +0900

    chore: adjust component-scan in api module to include application service

--- api/src/main/java/hanghae/api/ApiApplication.java
+++ api/src/main/java/hanghae/api/ApiApplication.java
@@ -2,8 +2,13 @@ package hanghae.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+        "hanghae.api",
+        "hanghae.application.adapter.inbound"
+})
 public class ApiApplication {
 
     public static void main(String[] args) {
