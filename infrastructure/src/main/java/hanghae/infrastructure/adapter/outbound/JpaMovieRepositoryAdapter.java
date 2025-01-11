3ca625bcd25b175b0db167e76643e5e7c53d639a
zun <k5orkrj@gmail.com>
2025-01-11 09:55:51 +0900

    feat: (temp) add logics to find movies playing and to convert result into domain object

--- infrastructure/src/main/java/hanghae/infrastructure/adapter/outbound/JpaMovieRepositoryAdapter.java
+++ infrastructure/src/main/java/hanghae/infrastructure/adapter/outbound/JpaMovieRepositoryAdapter.java
@@ -1,2 +1,31 @@
-package hanghae.infrastructure.adapter.outbound;public class JpaMovieRepositoryAdapter {
+package hanghae.infrastructure.adapter.outbound;
+
+import hanghae.domain.domain.Movie;
+import hanghae.domain.entity.MovieEntity;
+import hanghae.domain.port.outbound.MovieRepository;
+import hanghae.infrastructure.repository.JpaMovieRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class JpaMovieRepositoryAdapter implements MovieRepository {
+
+    private final JpaMovieRepository jpaMovieRepository;
+
+    @Override
+    public Optional<List<Movie>> findMoviesPlayingNow(LocalDate now) {
+        return jpaMovieRepository.findMoviesPlayingNow(now)
+                .map(this::toMovieList);
+    }
+
+    private List<Movie> toMovieList(List<MovieEntity> movieEntities) {
+        return movieEntities.stream()
+                .map(Movie::from)
+                .toList();
+    }
 }
--- infrastructure/src/main/java/hanghae/infrastructure/repository/JpaMovieRepository.java
+++ infrastructure/src/main/java/hanghae/infrastructure/repository/JpaMovieRepository.java
@@ -1,2 +1,12 @@
-package hanghae.infrastructure.repository;public interface JpaMovieRepository {
+package hanghae.infrastructure.repository;
+
+import hanghae.domain.entity.MovieEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+public interface JpaMovieRepository extends JpaRepository<MovieEntity, Long> {
+    Optional<List<MovieEntity>> findMoviesPlayingNow(LocalDate now);
 }
