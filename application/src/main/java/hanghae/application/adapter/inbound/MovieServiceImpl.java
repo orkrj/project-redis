ccf33e04076927894a1a63dcd0326c77b8582fa8
zun <k5orkrj@gmail.com>
2025-01-11 09:54:59 +0900

    feat: implement service

--- application/src/main/java/hanghae/application/adapter/inbound/MovieServiceImpl.java
+++ application/src/main/java/hanghae/application/adapter/inbound/MovieServiceImpl.java
@@ -1,2 +1,30 @@
-package hanghae.application.adapter.inbound;public class MovieServiceImpl {
+package hanghae.application.adapter.inbound;
+
+import hanghae.application.dto.MovieResponse;
+import hanghae.application.port.inbound.MovieService;
+import hanghae.common.exception.NoContentsException;
+import hanghae.domain.domain.Movie;
+import hanghae.domain.port.outbound.MovieRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MovieServiceImpl implements MovieService {
+
+    private final MovieRepository movieRepository;
+
+    @Override
+    public List<MovieResponse> findMoviesPlayingNow() {
+        // TODO findAll 같은 공통 로직으로 찾은 다음 stream 으로 필터링하는 방향으로 리팩토링
+        List<Movie> movieList = movieRepository.findMoviesPlayingNow(LocalDate.now())
+                .orElseThrow(() -> new NoContentsException("movie"));
+
+        return movieList.stream()
+                .map(MovieResponse::from)
+                .toList();
+    }
 }
