1e842579b20bdef34baf7b27f9ceed249e47aefd
zun <k5orkrj@gmail.com>
2025-01-18 16:43:43 +0900

    feat: add logics to get theater by movie_id

--- infrastructure/src/main/java/hanghae/infrastructure/repository/JpaMovieTheaterRepository.java
+++ infrastructure/src/main/java/hanghae/infrastructure/repository/JpaMovieTheaterRepository.java
@@ -1,4 +1,10 @@
 package hanghae.infrastructure.repository;
 
-public interface MovieTheaterRepository {
+import hanghae.infrastructure.domain.entity.MovieTheaterEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface JpaMovieTheaterRepository extends JpaRepository<MovieTheaterEntity, Long> {
+    List<MovieTheaterEntity> findMovieTheaterEntitiesByMovie_MovieId(Long movieId);
 }
