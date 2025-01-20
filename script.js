61638d52a85e7c798b21b27611fbd31c561d6733
zun <k5orkrj@gmail.com>
2025-01-20 18:02:37 +0900

    chore: update script.js for k6 test

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export default function() {
-    http.get("http://localhost:8081/api/v1/movie");
+    http.get("http://localhost:8080/api/v3/movie");
 }
